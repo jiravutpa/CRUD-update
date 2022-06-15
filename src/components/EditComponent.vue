35d3c79791c3cc2cbdd8f01a795320c68d68273f
manow49 <jiravutpa@kkumail.com>
2022-06-15 17:00:40 +0700

    update

---
+++ src/components/EditComponent.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="vld-parent">
+    <loading
+      v-model:active="isLoading"
+      :can-cancel="true"
+      :is-full-page="false"
+    />
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h1>Edit</h1>
+        <form @submit.prevent="handleSubmitForm">
+          <div class="form-group">
+            <label for="name">Brand (ID)</label>
+            <select v-model="user.brandId" class="form-select">
+              <option
+                v-for="(item, index) in optionBrands"
+                :key="index"
+                :value="item"
+              >
+                {{ item.brandName }}
+              </option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="name">Model Code</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="user.modelCode"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="user.name"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Model Year</label>
+            <input
+              type="number"
+              class="form-control"
+              v-model="user.modelYear"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="fuel">Fuel</label><br />
+            <div
+              v-for="(itemf, index) in optionFuel"
+              :key="index"
+              :value="itemf"
+            >
+              <!-- <select v-model="user.fuel" class="form-select">
+            <option v-for="(itemf, index) in optionFuel" :key="index" :value="itemf" >{{itemf.name}}</option> -->
+              <input
+                type="radio"
+                name="fuel"
+                :key="index"
+                :value="itemf"
+                v-model="user.fuel"
+              />
+              <a>{{ itemf.name }}</a>
+              <!-- </select> -->
+            </div>
+          </div>
+          <!-- <div class="form-group">
+          <label for="phone">Fuel</label>
+
+          <select v-model="user.fuel" class="form-select">
+            <option
+              v-for="(itemf, index) in optionFuel"
+              :key="index"
+              :value="itemf"
+            >
+              {{ itemf.name }}
+            </option>
+          </select>
+        </div> -->
+          <br />
+          <div class="form-group">
+            <button class="btn btn-success btn-block">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+// import Multiselect from "vue-multiselect";
+
+export default {
+  //   components: { Multiselect },
+  created() {
+    this.showModel();
+    this.op();
+  },
+  data() {
+    return {
+      isLoading: false,
+      optionBrands: [],
+      optionFuel: [
+        {
+          id: "D",
+          name: "ดีเซล",
+        },
+        {
+          id: "S",
+          name: "เบนซิน",
+        },
+      ],
+
+      user: {
+        brandId: "",
+        modelCode: "",
+        name: "",
+        modelYear: "",
+        fuel: "",
+      },
+    };
+  },
+  components: {
+    Loading,
+  },
+  methods: {
+    logModel() {
+      console.log(this.user.brandId);
+    },
+    handleSubmitForm() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1000);
+      // console.log(this.user.brandId.brandId);
+      // console.log(this.user.fuel.id);
+      let apiURL =
+        "https://dms-backend-dev-dxvb7izyka-as.a.run.app/api/vehicle-model/update";
+      axios
+        .put(apiURL, {
+          modelId: this.$route.params.id,
+          brandId: this.user.brandId.brandId,
+          modelCode: this.user.modelCode,
+          name: this.user.name,
+          modelYear: this.user.modelYear,
+          fuel: this.user.fuel.id,
+        })
+        .then(() => {
+          this.$router.push("/view");
+          this.user = {
+            brandId: "",
+            modelCode: "",
+            name: "",
+            modelYear: "",
+            fuel: "",
+          };
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    op() {
+      axios
+        .get(
+          "https://dms-backend-dev-dxvb7izyka-as.a.run.app/api/vehicle-model/brand",
+          {
+            params: {},
+          }
+        )
+        .then((res) => {
+          const arr = [];
+          res.data.forEach((value) => {
+            arr.push(value);
+            console.log(value);
+          });
+          this.optionBrands = arr;
+
+          console.log(arr);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    showModel() {
+      // console.log(this.$route.params);
+      axios
+        .get(
+          "https://dms-backend-dev-dxvb7izyka-as.a.run.app/api/vehicle-model/show",
+          {
+            params: {
+              modelId: this.$route.params.id,
+            },
+          }
+        )
+        .then((res) => {
+          this.user.brandId = {
+            brandId: res.data.data.brandId,
+            brandName: res.data.data.brandName,
+          };
+          this.user.modelCode = res.data.data.modelCode;
+          this.user.name = res.data.data.name;
+          this.user.modelYear = res.data.data.modelYear;
+          this.user.fuel = {
+            id: res.data.data.fuel,
+            name: res.data.data.fuel == "D" ? "ดีเซล" : "เบนซิน",
+          };
+        })
+        .catch((error) => {
+          console.log("TT");
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
---
+++ src/components/MaTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="vld-parent">
+    <loading
+      v-model:active="isLoading"
+      :can-cancel="true"
+      :on-cancel="onCancel"
+      :is-full-page="false"
+    />
+
+    <button @click.prevent="doAjax">fetch Data</button>
+  </div>
+</template>
+
+<script>
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  components: {
+    Loading,
+  },
+  methods: {
+    doAjax() {
+      this.isLoading = true;
+      // simulate AJAX
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1000);
+    },
+    onCancel() {
+      console.log("User cancelled the loader.");
+    },
+  },
+};
+</script>
